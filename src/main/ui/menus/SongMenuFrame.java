61338632369d41611cf2c642e3726bb64e4e26de
Julia <juliayou604@gmail.com>
2022-08-04 20:16:54 -0700

    setup playlist songs menu display

--- src/main/ui/menus/SongMenuFrame.java
+++ src/main/ui/menus/SongMenuFrame.java
@@ -19,22 +19,33 @@ public class SongMenuFrame extends JFrame implements ActionListener, ListSelecti
 
     private JButton playButton;
     private JButton pauseButton;
-    private JScrollPane scrollPanel;
+    private JButton addSongButton;
+    private JButton deleteSongButton;
 
+    private JPanel mainPanel;
+    private JPanel sidePanel;
     private JPanel bottomMainPanel;
     private JPanel playPausePanel;
 
+
+
     private static final int WIDTH = 800;
     private static final int HEIGHT = 550;
-
 
     private final JMenuBar menuBar;
     private final JMenu file;
     private final JMenuItem mainMenu;
     private final JMenuItem back;
 
-    private final JList<String> list;
-    private final DefaultListModel<String> listModel;
+    private final JList<String> mainSongList;
+    private final DefaultListModel<String> mainSongListModel;
+
+    private final JList<String> songsToCopyList;
+    private final DefaultListModel<String> songsToCopyListModel;
+
+    private JScrollPane scrollPanelLeft;
+    private JScrollPane scrollPanelRight;
+
     private final MusicApp app;
     private final Playlist playlist;
 
@@ -61,22 +72,85 @@ public class SongMenuFrame extends JFrame implements ActionListener, ListSelecti
 
         frame.setJMenuBar(menuBar);
 
-        listModel = new DefaultListModel<>();
-
+        mainSongListModel = new DefaultListModel<>();
         for (Song s : playlist.getSongsInPlaylist()) {
             String title = s.getSongTitle();
-            listModel.addElement(title);
+            mainSongListModel.addElement(title);
         }
 
         //Create the list and put it in a scroll pane.
-        list = new JList<>(listModel);
-        list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        list.setSelectedIndex(0);
-        list.addListSelectionListener(this);
-        list.setVisibleRowCount(5);
-        list.setFont(new Font("Serif", Font.PLAIN, 14));
-        scrollPanel = new JScrollPane(list);
-        scrollPanel.setPreferredSize(new Dimension((int) (WIDTH * 0.77), (int) (HEIGHT * 0.7)));
+        mainSongList = new JList<>(mainSongListModel);
+        mainSongList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        mainSongList.setSelectedIndex(0);
+        mainSongList.addListSelectionListener(this);
+        mainSongList.setVisibleRowCount(5);
+        mainSongList.setFont(new Font("Serif", Font.PLAIN, 14));
+        scrollPanelLeft = new JScrollPane(mainSongList);
+        scrollPanelLeft.setPreferredSize(new Dimension((int) (WIDTH * 0.47), (int) (HEIGHT * 0.7)));
+
+        songsToCopyListModel = new DefaultListModel<>();
+        for (Song s : app.getAllSongs()) {
+            if (!playlist.getSongsInPlaylist().contains(s.getSongTitle())) {
+                songsToCopyListModel.addElement(s.getSongTitle());
+            }
+        }
+
+        //Create the list and put it in a scroll pane.
+        songsToCopyList = new JList<>(songsToCopyListModel);
+        songsToCopyList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+        songsToCopyList.setSelectedIndex(0);
+        songsToCopyList.addListSelectionListener(this);
+        songsToCopyList.setVisibleRowCount(5);
+        songsToCopyList.setFont(new Font("Serif", Font.PLAIN, 14));
+        scrollPanelRight = new JScrollPane(songsToCopyList);
+        scrollPanelRight.setPreferredSize(new Dimension((int) (WIDTH * 0.33), (int) (HEIGHT * 0.7)));
+
+        JLabel songsAdded = new JLabel("Songs in your playlist");
+        songsAdded.setFont(new Font("Serif", Font.PLAIN, 14));
+        JLabel songsNotAdded = new JLabel("Choose songs to add");
+        songsNotAdded.setFont(new Font("Serif", Font.PLAIN, 14));
+
+
+        addSongButton = new JButton("Add songs");
+        addSongButton.setPreferredSize(new Dimension((int) (WIDTH * 0.17), (int) (HEIGHT * .32)));
+        addSongButton.setFont(new Font("Serif", Font.PLAIN, 14));
+        addSongButton.addActionListener(this);
+        addSongButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        deleteSongButton = new JButton("Delete songs");
+        deleteSongButton.setPreferredSize(new Dimension((int) (WIDTH * .17), (int) (HEIGHT * .32)));
+        deleteSongButton.setFont(new Font("Serif", Font.PLAIN, 14));
+        deleteSongButton.addActionListener(this);
+        deleteSongButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JPanel emptyPanel = new JPanel();
+        emptyPanel.setPreferredSize(new Dimension((int) (WIDTH * 0.17), (int) (HEIGHT * .06)));
+
+        sidePanel = new JPanel();
+        sidePanel.setPreferredSize(new Dimension((int) (WIDTH * 0.2), (int) (HEIGHT * .7)));
+        sidePanel.add(emptyPanel);
+        sidePanel.add(addSongButton);
+        sidePanel.add(deleteSongButton);
+        sidePanel.setAlignmentY(Component.CENTER_ALIGNMENT);
+
+        JPanel subMainPanelLeft = new JPanel();
+        subMainPanelLeft.setPreferredSize(new Dimension((int) (WIDTH * 0.47), (int) (HEIGHT * .7)));
+        subMainPanelLeft.setAlignmentY(Component.CENTER_ALIGNMENT);
+        subMainPanelLeft.add(songsAdded, BorderLayout.NORTH);
+        subMainPanelLeft.add(scrollPanelLeft, BorderLayout.SOUTH);
+
+        JPanel subMainPanelRight = new JPanel();
+        subMainPanelRight.setPreferredSize(new Dimension((int) (WIDTH * 0.33), (int) (HEIGHT * .7)));
+        subMainPanelRight.setAlignmentY(Component.CENTER_ALIGNMENT);
+        subMainPanelRight.add(songsNotAdded, BorderLayout.NORTH);
+        subMainPanelRight.add(scrollPanelRight, BorderLayout.SOUTH);
+
+        mainPanel = new JPanel();
+        mainPanel.add(subMainPanelLeft, BorderLayout.WEST);
+        mainPanel.add(subMainPanelRight, BorderLayout.EAST);
+//        mainPanel.add(songsNotAdded, BorderLayout.NORTH);
+//        mainPanel.add(scrollPanelRight, BorderLayout.EAST);
+
 
         playPausePanel = new JPanel();
         playPausePanel.setPreferredSize(new Dimension(WIDTH, (int) (HEIGHT * .17)));
@@ -88,7 +162,8 @@ public class SongMenuFrame extends JFrame implements ActionListener, ListSelecti
         bottomMainPanel.add(playPausePanel);
 
         frame.add(bottomMainPanel, BorderLayout.SOUTH);
-        frame.add(scrollPanel, BorderLayout.NORTH);
+        frame.add(mainPanel, BorderLayout.WEST);
+        frame.add(sidePanel, BorderLayout.EAST);
     }
 
 
@@ -145,22 +220,22 @@ public class SongMenuFrame extends JFrame implements ActionListener, ListSelecti
     // https://docs.oracle.com/javase/tutorial/uiswing/examples/components/ListDemoProject/src/components/ListDemo.java
     class DeleteButtonListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            int index = list.getSelectedIndex();
-            listModel.remove(index);
+            int index = mainSongList.getSelectedIndex();
+            mainSongListModel.remove(index);
 //            saveLoadLabel.setText("");
 
-            int size = listModel.getSize();
+            int size = mainSongListModel.getSize();
 
             if (size == 0) {
 //                deleteButton.setEnabled(false);
 
             } else {
-                if (index == listModel.getSize()) {
+                if (index == mainSongListModel.getSize()) {
                     index--;
                 }
 
-                list.setSelectedIndex(index);
-                list.ensureIndexIsVisible(index);
+                mainSongList.setSelectedIndex(index);
+                mainSongList.ensureIndexIsVisible(index);
             }
         }
     }
@@ -188,7 +263,7 @@ public class SongMenuFrame extends JFrame implements ActionListener, ListSelecti
 //            saveLoadLabel.setText("");
 
 
-            int index = list.getSelectedIndex(); //get selected index
+            int index = mainSongList.getSelectedIndex(); //get selected index
             if (index == -1) { //no selection, so insert at beginning
                 index = 0;
             } else {           //add after the selected item
@@ -200,8 +275,8 @@ public class SongMenuFrame extends JFrame implements ActionListener, ListSelecti
 //            inputPlaylistNameForm.setText("");
 
             //Select the new item and make it visible.
-            list.setSelectedIndex(index);
-            list.ensureIndexIsVisible(index);
+            mainSongList.setSelectedIndex(index);
+            mainSongList.ensureIndexIsVisible(index);
         }
 
 //        protected boolean alreadyInList(String name) {
